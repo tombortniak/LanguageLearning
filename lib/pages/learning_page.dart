d0b5e6bdc28c923a1eb1dd5a93b696b0384ce2fb
tombortniak <tombortniak@gmail.com>
2022-04-03 11:15:47 +0200

    add fields validation

--- lib/pages/learning_page.dart
+++ lib/pages/learning_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:language_learning/pages/question_page.dart';
+import 'package:language_learning/pages/learning_results_page.dart';
 import 'package:language_learning/constants.dart';
 import 'package:tuple/tuple.dart';
 import 'package:language_learning/database/database.dart';
@@ -14,21 +14,83 @@ class LearningPage extends StatefulWidget {
 }
 
 class _LearningPageState extends State<LearningPage> {
-  dynamic currentElement;
+  final formKey = GlobalKey<FormState>();
+  int currentElementIndex = 0;
+  int fieldsNumber = 0;
   List<dynamic> learnedElements = [];
   List<dynamic> otherElements = [];
+  List<TextEditingController> textControllers =
+      List.generate(7, (index) => TextEditingController());
+  List<Color> fieldsColors = List.generate(7, (index) => Colors.deepPurple);
   int errors = 0;
+  String buttonText = 'Sprawdź';
+  bool isCurrentCorrect = false;
 
   @override
   void initState() {
     widget.learningContent.shuffle();
-    currentElement = widget.learningContent.first;
     otherElements.addAll(widget.learningContent);
     super.initState();
   }
 
+  void nextElement() {
+    learnedElements.add(widget.learningContent[currentElementIndex]);
+    otherElements.remove(widget.learningContent[currentElementIndex]);
+    currentElementIndex++;
+    for (var controller in textControllers) {
+      controller.clear();
+    }
+    for (int i = 0; i < fieldsColors.length; ++i) {
+      fieldsColors[i] = Colors.deepPurple;
+    }
+    isCurrentCorrect = false;
+    setState(() {});
+  }
+
+  bool checkAnswer() {
+    var element = widget.learningContent[currentElementIndex];
+    if (textControllers[0].text !=
+        widget.learningContent[currentElementIndex].translation) {
+      fieldsColors[0] = Colors.redAccent;
+      setState(() {});
+      return false;
+    }
+
+    if (element is Verb) {
+      if (textControllers[1].text != element.firstPersonSingular) {
+        return false;
+      }
+
+      if (textControllers[2].text != element.secondPersonSingular) {
+        return false;
+      }
+
+      if (textControllers[3].text != element.thirdPersonSingular) {
+        return false;
+      }
+
+      if (textControllers[4].text != element.firstPersonPlural) {
+        return false;
+      }
+
+      if (textControllers[5].text != element.secondPersonPlural) {
+        return false;
+      }
+
+      if (textControllers[6].text != element.thirdPersonPlural) {
+        return false;
+      }
+    }
+
+    for (int i = 0; i < fieldsColors.length; ++i) {
+      fieldsColors[i] = Colors.greenAccent;
+    }
+    buttonText = 'Dalej';
+    return true;
+  }
+
   Form buildForm() {
-    int fieldsNumber = currentElement is Verb ? 7 : 1;
+    fieldsNumber = currentElementIndex is Verb ? 7 : 1;
     int personConjugation = 1;
     double width;
     String hint;
@@ -48,9 +110,10 @@ class _LearningPageState extends State<LearningPage> {
             Size(width, 60),
           ),
           child: TextFormField(
+            controller: textControllers[i],
             decoration: InputDecoration(
-              focusedBorder: const UnderlineInputBorder(
-                borderSide: BorderSide(color: Colors.deepPurple),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: fieldsColors[i]),
               ),
               hintText: hint,
               hintStyle: Theme.of(context).textTheme.bodyText2,
@@ -66,10 +129,11 @@ class _LearningPageState extends State<LearningPage> {
     }
 
     return Form(
+      key: formKey,
       child: Column(
         children: [
           boxes[0],
-          if (currentElement is Verb)
+          if (currentElementIndex is Verb)
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -95,14 +159,21 @@ class _LearningPageState extends State<LearningPage> {
   }
 
   @override
+  void dispose() {
+    for (var controller in textControllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final mainWidth = MediaQuery.of(context).size.width * .6;
-    final width = MediaQuery.of(context).size.width * .4;
     return Scaffold(
       appBar: AppBar(),
       body: Container(
         margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -117,31 +188,55 @@ class _LearningPageState extends State<LearningPage> {
                     style: Theme.of(context).textTheme.bodyText1)
               ],
             ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      Center(
-                        child: Text(
-                          currentElement.content,
-                          style: Theme.of(context).textTheme.headline4,
-                        ),
-                      ),
-                      buildForm()
-                    ],
+            Column(
+              children: [
+                Center(
+                  child: Text(
+                    widget.learningContent[currentElementIndex].content,
+                    style: Theme.of(context).textTheme.headline4,
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Sprawdź'),
-                  )
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                buildForm(),
+              ],
             ),
+            isCurrentCorrect
+                ? ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.greenAccent,
+                    ),
+                    onPressed: () {
+                      nextElement();
+                    },
+                    child: Text(
+                      buttonText,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1!
+                          .copyWith(color: Colors.black),
+                    ),
+                  )
+                : ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if (checkAnswer()) {
+                          if (currentElementIndex ==
+                              widget.learningContent.length - 1) {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const LearningResultsPage()));
+                          } else {
+                            isCurrentCorrect = true;
+                          }
+                        }
+                      });
+                    },
+                    child: Text(buttonText),
+                  )
           ],
         ),
       ),
